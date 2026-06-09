958a4bef01c1c65f07c682f411643ae68a8a298b
Max Thannheimer <max.thannheimer@mathematik.uni-stuttgart.de>
2026-06-09 16:44:35 +0200

    Started to implement double parameter MCMC
    i.e., we sample the same gaussian random fields for the old and the proposed parameter and then do the transfomations accordingly. thereby we hope to lessen stochastic fluctuations.

--- src/MCMC.jl
+++ src/MCMC.jl
@@ -53,7 +53,7 @@ function log_likehood_log_gauss_1d_non_normalized(x,mu,sigma)
     (-0.5*(log(x)-mu)^2/sigma^2)
 end
 
-
+""" Main functions for MCMC algorithm """
 
 """ standard procedure with no clever tricks, only use of modification use of img and real part in fbm simulation"""
 function MCMC_basic(;N_MCMC::Int,observation::Observation,threshold::Float64,param::Parameter, grid::Grid,N_est_c::Int,N_cond_sim::Int,N_est_d::Int)
@@ -111,6 +111,7 @@ function MCMC_basic(;N_MCMC::Int,observation::Observation,threshold::Float64,par
     Dict( "param" => param_vec, "Number of exceedance"=> number_exceed_vec, "log_likelihood" => log_likelihood_vec, "res_ell_X" => res_ell_X_vec) # "log_likelihood_diff" => log_likelihood_diff_vec, "log_likelihood_diff_short" => log_likelihood_diff_vec_short)
 end  
 
+""" MCMC procedure using real and imaginary part in fbm simulation, reusing exceedance samples for l4 calculation, and using a variable sample size depending on the sample variance for l2 calculation to save time """
 
 function MCMC_(;N_MCMC::Int,observation::Observation,threshold::Float64,param::Parameter, grid::Grid,N_est_c::Int,N_cond_sim::Int,N_est_d::Int)
     quot_bound=0.05
@@ -170,6 +171,67 @@ function MCMC_(;N_MCMC::Int,observation::Observation,threshold::Float64,param::P
 end  
 
 
+""" MCMC procedure also using same gaussian samples to construct the proposed param likelihood and the current one, rest as above, i.e. using real and imaginary part in fbm simulation, reusing exceedance samples for l4 calculation, and using a variable sample size depending on the sample variance for l2 calculation to save time """
+
+function MCMC_double_param(;N_MCMC::Int,observation::Observation,threshold::Float64,param::Parameter, grid::Grid,N_est_c::Int,N_cond_sim::Int,N_est_d::Int)
+    quot_bound=0.05
+    N_est_c_lim=2_500_000
+    num_obs=size(observation.obs_x0,1)
+
+    param_vec = [Parameter(α=NaN, β=NaN, c=NaN) for i=1:N_MCMC+1]
+    param_vec[1]=param
+    number_exceed_vec = [NaN for i=1:N_MCMC]
+    log_likelihood_vec = [NaN for i=1:N_MCMC]
+    res_ell_X_vec = [[NaN for i in 1:num_obs] for j in 1:N_MCMC] 
+    
+    for trial in 1:N_MCMC
+
+        #propose new params
+        eps_beta=0.05 # 0.05 -> 0.025
+        eps_c=0.1 #0.1 -> 0.05
+        eps_alpha=0.1 #0.1 -> 0.05
+        beta_eps,old_interval,new_interval=uniform_proposal(param_vec[trial].β,eps_beta,0.0,2.0)
+        param_eps=Parameter(c=gaussian_proposal(param_vec[trial].c,eps_c),β=beta_eps,α=gaussian_proposal(param_vec[trial].α,eps_alpha))
+
+        (exceedance_observation, res_ell_X_vec[trial],l4) = exceed_cond_sim_and_l4(num_runs=N_cond_sim, observation=observation, threshold=threshold, param=param_vec[trial], grid=grid )
+
+       
+
+      
+        l1=l_1_fun(param=param_vec[trial], grid=grid,exceedance_observation=exceedance_observation)   
+        l2=l_2_fun(param=param_vec[trial], grid=grid, N_est_c=N_est_c, N_est_c_lim=N_est_c_lim, quot_bound=quot_bound, exceedance_observation=exceedance_observation)
+        l3=l_3_fun(exceedance_observation=exceedance_observation, param=param_vec[trial], threshold=1.0)
+        #l4=l_4_fun(exceedance_observation=exceedance_observation, threshold=1.0, param=param_vec[trial], grid=grid ,N_est_d=N_est_d)
+        #l4 is already calculated in the exceed_cond_sim_and_l4 function, so we can save some time by not calculating it again here
+
+
+
+
+        prior = log_likehood_log_gauss_1d_non_normalized(param_vec[trial].c,0.0,1.5)+log_likehood_log_gauss_1d_non_normalized(param_vec[trial].α,0.0,1.0)
+        log_likelihood_old=sum([l1,l2,l3,l4,prior,-log(old_interval)]) #-log interval length of uniform proposal for beta
+      
+        l1=l_1_fun(param=param_eps, grid=grid,exceedance_observation=exceedance_observation)   
+        l2=l_2_fun(param=param_eps, grid=grid, N_est_c=N_est_c, N_est_c_lim=N_est_c_lim, quot_bound=quot_bound, exceedance_observation=exceedance_observation)
+        l3=l_3_fun(exceedance_observation=exceedance_observation, param=param_eps, threshold=1.0)
+        l4=l_4_fun(exceedance_observation=exceedance_observation, threshold=1.0, param=param_eps, grid=grid ,N_est_d=N_cond_sim+1)
+
+
+
+
+        prior = log_likehood_log_gauss_1d_non_normalized(param_eps.c,0.0,1.5)+log_likehood_log_gauss_1d_non_normalized(param_eps.α,0.0,1.0)
+
+        log_likelihood_new=sum([l1,l2,l3,l4,prior,-log(new_interval)]) #-log interval length of uniform proposal for beta
+
+        #MCMC update of param according to acceptance rate calculated with old and new likelihoods
+        #update and safe param after MCMC step
+        param_vec[trial+1],log_likelihood_vec[trial] =parameter_update(param_old=param_vec[trial],param_new=param_eps,log_likelihood_old=log_likelihood_old,log_likelihood_new=log_likelihood_new)
+        number_exceed_vec[trial]=size(exceedance_observation.obs_x0,1)
+    end
+    println("MCMC finished")
+    Dict( "param" => param_vec, "Number of exceedance"=> number_exceed_vec, "log_likelihood" => log_likelihood_vec, "res_ell_X" => res_ell_X_vec) # "log_likelihood_diff" => log_likelihood_diff_vec, "log_likelihood_diff_short" => log_likelihood_diff_vec_short)
+end  
+
+""" MCMC procedure using aproximated risk functional """
 function MCMC_approx_(;N_MCMC::Int,observation::Observation,threshold::Float64,param::Parameter, grid::Grid,N_est_c::Int,N_cond_sim::Int,N_est_d::Int)
     num_obs=size(observation.obs_x0,1)
 
--- src/fractionalBM.jl
+++ src/fractionalBM.jl
@@ -197,3 +197,82 @@ function FBM_simu_fast(;param::Parameter,grid::Grid,num_sim::Int)::Vector{Vector
     c_sqrt.*res1
 end
 
+""" Function to generate a fractional Brownian motion samples using circulant embedding method """
+""" sampling for proposed and current param with the same gaussian samples to prevent influence of sampling variability on acceptance decision, i.e. using same gaussian samples to construct the proposed param likelihood and the current one """
+""" and like above dependent simulations, i.e. both the real and the imaginary part of the gaussian are used, but they are dependend, so not twice as many simulations for free, but more efficient than independent simulations """ 
+function FBM_simu_fast_double_param(;param_a::Parameter,param_b::Parameter, grid::Grid,num_sim::Int)::Tuple{Vector{Vector{Float64}},Vector{Vector{Float64}}}
+    gridsize=grid.gridsize
+    β_max=max(param_a.β,param_b.β)
+    c_sqrt_a=sqrt(param_a.c)
+    c_sqrt_b=sqrt(param_b.c)
+    H_a=param_a.β/2 #Hurst Params
+    H_b=param_b.β/2
+    if β_max<1.1 #TODO: check if this is correct, literature says a<=1.5, we used a<=1.1
+        R=1
+    else
+        R=2 #expanded gridsize, region of interest is [0,1]×[0.1]
+    end
+    n=m=R*gridsize #size of grid is m ∗ n, cov mat size is n^2*m^2
+    tx=(1:n)/n*R; ty=(1:m)/m*R #grid points in x and y 
+    Rows_a=zeros(m,n)
+    Rows_b=zeros(m,n)
+    for i in 1:n 
+        for j in 1:m
+            Rows_a[j,i]=rho([tx[i],ty[j]],[tx[1],ty[1]],R,2*H_a)[1]
+            Rows_b[j,i]=rho([tx[i],ty[j]],[tx[1],ty[1]],R,2*H_b)[1]
+        end
+    end
+    BlkCirc_row_a=[Rows_a  Rows_a[:,end-1:-1:2] ;  Rows_a[end-1:-1:2,:]  Rows_a[end-1:-1:2,end-1:-1:2 ]  ]
+    BlkCirc_row_b=[Rows_b  Rows_b[:,end-1:-1:2] ;  Rows_b[end-1:-1:2,:]  Rows_b[end-1:-1:2,end-1:-1:2 ]  ]
+    #calculate eigenvalues via fft
+    eig_vals_a=real(fft2(BlkCirc_row_a)/(4*(m-1)*(n-1)))
+    eig_vals_b=real(fft2(BlkCirc_row_b)/(4*(m-1)*(n-1)))
+    #optional:
+    #set small values to zero:
+    #eps = 10^-8
+    #eig_vals=eig_vals.*(abs.(eig_vals) .> eps)
+    #eig_vals=eig_vals.*(eig_vals .>= 0)
+    eig_vals_a=sqrt.(eig_vals_a)
+    eig_vals_b=sqrt.(eig_vals_b)
+
+    res_a=[Vector{Float64}(undef,gridsize*gridsize) for i in 1:num_sim]
+    res_b=[Vector{Float64}(undef,gridsize*gridsize) for i in 1:num_sim]
+    #res2=[Vector{Float64}(undef,gridsize*gridsize) for i in 1:num_sim]
+    #one can get two times as many simulations for free by using the imaginary and real part 
+    #of the complex gaussian, but they are dependend
+    
+    #res2=[Matrix{Float64}(undef,gridsize,gridsize) for i in 1:num_sim]
+    
+    for trial in 1:(num_sim÷2)
+        #generate field with covariance given by block circulant matrix
+        Z= randn(2*(m-1),2*(n-1)) + im* randn(2*(m-1),2*(n-1)) 
+        #fft to calculate diag*Z
+        F_a=fft2(eig_vals_a.*Z)
+        F_b=fft2(eig_vals_b.*Z)
+        #extract subblock with desired cov variance
+        F_a=F_a[1:gridsize,1:gridsize]
+        F_b=F_b[1:gridsize,1:gridsize]
+        (out_a,c_0_a,c_2_a)=rho([0,0],[0,0],R,2*H_a)
+        (out_b,c_0_b,c_2_b)=rho([0,0],[0,0],R,2*H_b)
+        #optional two (dependend) real fields
+        field1_a=real(F_a)
+        field2_a=imag(F_a)
+        field1_b=real(F_b)
+        field2_b=imag(F_b)
+        #set field zero at origin
+        field1_a=field1_a.-field1_a[1,1]
+        field2_a=field2_a.-field2_a[1,1]
+        field1_b=field1_b.-field1_b[1,1]
+        field2_b=field2_b.-field2_b[1,1]
+        #TODO
+        #make correction for embedding with a term c_2*r^2
+        X_grid_comp = [i for i in tx[1:gridsize], j in tx[1:gridsize]].-tx[1]
+        Y_grid_comp = [j for i in tx[1:gridsize], j in tx[1:gridsize]].-tx[1]
+    
+        res_a[trial]=vec((field1 + (X_grid_comp*randn()+Y_grid_comp*randn() ) .*sqrt(2*c_2))')
+        res_a[trial+num_sim÷2]=vec((field2 + (X_grid_comp*randn()+Y_grid_comp*randn() ) .*sqrt(2*c_2))')
+    end
+
+    #(c_sqrt.*res1,c_sqrt.*res2)
+    c_sqrt_a.*res_a
+end
